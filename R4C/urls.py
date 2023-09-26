5504fb3692fc7c3ad9a831eba725d97935e28a13
Maxim <fangsofthemoon@yandex.ru>
2023-09-26 15:03:50 +0300

    solved task#2

--- R4C/urls.py
+++ R4C/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from robots.views import create_robot
+from robots.views import create_robot, summary
+from robots import views
 
 urlpatterns = [
     path('create_robot/', create_robot, name='create_robot'),
+    path('summary/', views.summary, name='summary'),
+    path('download_summary_excel/', views.download_excel_report, name='download_summary_excel'),
     path('admin/', admin.site.urls),
 ]
--- robots/models.py
+++ robots/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+import datetime
 
 class Robot(models.Model):
     serial = models.CharField(max_length=5, blank=False, null=False, verbose_name='Серия')
     model = models.CharField(max_length=2, blank=False, null=False, verbose_name='Модель')
     version = models.CharField(max_length=2, blank=False, null=False, verbose_name='Версия')
-    created = models.DateTimeField(blank=False, null=False, auto_now_add=True, verbose_name='Создан')
+    created = models.DateTimeField(blank=False, null=False, default=datetime.datetime.now, verbose_name='Создан')
 
     def __str__(self):
         return f"{self.model}-{self.version}"
